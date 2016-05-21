ebd4677edab38893a25e8e8fd3727f27910fc7ad
JohnSnoow <jean-bezet@orange.fr>
2016-05-21 18:54:17 +0200

    Step 3

---
+++ POO Project hight scores/src/scores/HighScore3.java
@@ -0,0 +1,84 @@
+package scores;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HighScore3 {
+	
+	/**
+	 * This method opens an HTTP connection with the ThingSpeak web site site and asks for scores.
+	 * @return containing the different interesting lines of result
+	 */
+	public List<String> getScores() {
+		List<String> highScoresList = new ArrayList<>();	
+		try {
+			URL channelFeedURL = new URL("https://api.thingspeak.com/channels/109322/feeds.csv");
+			InputStream scores = channelFeedURL.openStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(scores));
+			// Skipping the first line.
+			reader.readLine();
+			String readString = reader.readLine();
+			while (readString != null) {
+				if (readString.length() > 0) {
+					highScoresList.add(readString);
+				}
+				readString = reader.readLine();
+			} // End While 
+		} 
+		catch (Exception e1) {
+			e1.printStackTrace();
+		}	
+		return highScoresList;
+	}//End getScores()
+	
+	/**
+	 * Take the list from the URL and parse each line to keep only the name and the score.
+	 * @param readScores List with all the lines from the csv file.
+	 * @return An array with the ten best players of the game. The best score is in first.
+	 */
+	public BestPlayer[] tenBestScores(List<String> readScores) {
+		
+		List<BestPlayer> allBest = new ArrayList<>();	// All scores from the URL parsed
+		BestPlayer[] top10 = new BestPlayer[10];	// The 10 best scores
+		
+		//For each line in readScore, we insert the name and the score of the player of the line in allBest
+		String[] line;
+		for (int i = 0; i<readScores.size(); i++) 
+		{
+			line = readScores.get(i).split(",");
+			String nomPlayer = line[3];
+			int scorePlayer = Integer.parseInt(line[2]);
+			BestPlayer player = new BestPlayer(nomPlayer,scorePlayer);
+			allBest.add(player);
+		}
+		
+		// Order allBest by score with the best player in first.
+		Collections.sort(allBest, Collections.reverseOrder());
+		
+		for (int i = 0; i <10; i++) {
+			top10[i] = allBest.get(i); 
+		}
+
+		return top10; 
+		
+		}
+	
+	public static void sendScore (BestPlayer p) throws IOException
+	{
+		String name = p.getPlayer();
+		String score = Integer.toString(p.getScore());
+		URL updateURL = new URL("https://api.thingspeak.com/update?api_key=7W2ZOVJ5U8YDLILB&field1=" + score + "&field2=" + name);
+		HttpURLConnection connection;
+		connection = (HttpURLConnection) updateURL.openConnection();
+		connection.setRequestMethod("GET");
+		connection.getResponseCode();
+	}
+
+}
--- POO Project hight scores/src/scores/TestHighScore3.java
+++ POO Project hight scores/src/scores/TestHighScore3.java
@@ -7,15 +7,16 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
-public class TestHighScore2 {
+public class TestHighScore3 {
 	
 static final String filePath = "data/scoreSamples.txt";
 	
 	/**
 	 * Step 1 and step2.
 	 * @param args0
+	 * @throws IOException 
 	 */
-	public static void main(String[] args0) {
+	public static void main(String[] args0) throws IOException {
 		
 		// Call getScores and print lines
 		HighScore1 highScore = new HighScore1();
@@ -29,33 +30,48 @@ static final String filePath = "data/scoreSamples.txt";
 		// Ask the name of the player
 		Scanner nameScanner = new Scanner(System.in);
 		System.out.println("Please write the name of the player : ");
-		String namePlayer = nameScanner.nextLine();
+		String namePlayer = nameScanner.nextLine();		
 		nameScanner.close();
+		//random score for the step 3
+		int score = (int) (Math.random() * 1000);
+
 		
+		nameScanner.close();
 		// reads all fake scores from the file
 		Path file = Paths.get(filePath);
 		try {
 			
 			List<String> scores = Files.readAllLines(file);
-			int random = (int) (Math.random() * scores.size());
+			/*int random = (int) (Math.random() * scores.size());
 			String Score = scores.get(random);
 			System.out.println("The player " + namePlayer + " has a score of "+ Score);
-		
+			*/
 		} catch (IOException e) { 
 
 			e.printStackTrace();
 		}
 		
 		// Create the top10 array, with the best player in first.
-		HighScore2 highScore2 = new HighScore2();
-		List<String> highScoresArray = highScore2.getScores();
+		HighScore3 highScore3 = new HighScore3();
+		List<String> highScoresArray = highScore3.getScores();
 		BestPlayer[] top10 = new BestPlayer[10];
-		top10 = highScore2.tenBestScores(highScoresArray);
-		
+		top10 = highScore3.tenBestScores(highScoresArray);
+		boolean testQualified = false;
 		// Print the ten best lines.
 		for(int j = 0 ; j < top10.length; j++) {
+			if (score >= top10[j].getScore())
+			{
+				testQualified = true;
+			}
 			System.out.println(top10[j].getScore() + " " + top10[j].getPlayer());
 		}
+		if (testQualified == true) 
+		{
+			System.out.println("You are one of the 10 best players ! ");
+			BestPlayer newBest = new BestPlayer (namePlayer, score);
+			HighScore3.sendScore(newBest);
+		}
+		
 
 	} // main
 
